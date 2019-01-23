790bf4359ed609e5e2a8b456d23b5f0d42be4d5e
segger <johanna.lynn.lahti@gmail.com>
2019-01-23 22:36:05 +0100

    Day8, star2

--- src/se/johannalynn/adventofcode/y2018/Day8.kt
+++ src/se/johannalynn/adventofcode/y2018/Day8.kt
@@ -7,17 +7,18 @@ object Day8 {
     fun main(args: Array<String>) {
         val scanner = Day.start(8, false)
 
-        star1(scanner)
+        //star1(scanner)
+        star2(scanner)
     }
 
-    private fun readNode(scanner: Scanner): Int {
+    private fun readMetadataSum(scanner: Scanner): Int {
         var metadata = 0
         val nbrOfChildren = scanner.nextInt()
         val nbrOfMetadata = scanner.nextInt()
         //println("Node " + nbrOfChildren + ", " + nbrOfMetadata)
 
         repeat(nbrOfChildren) {
-            metadata += readNode(scanner)
+            metadata += readMetadataSum(scanner)
         }
 
         repeat(nbrOfMetadata) {
@@ -30,7 +31,43 @@ object Day8 {
     }
 
     private fun star1(scanner: Scanner) {
-        val sum = readNode(scanner)
+        val sum = readMetadataSum(scanner)
         println(sum)
+    }
+
+    data class Node(val value: Int, val children: List<Node>)
+
+    private fun readRefValue(scanner: Scanner): Node {
+        val nbrOfChildren = scanner.nextInt()
+        val nbrOfMetadata = scanner.nextInt()
+
+        val children = ArrayList<Node>()
+        for(idx in 1..nbrOfChildren) {
+            val childNode = readRefValue(scanner)
+            children.add(childNode)
+        }
+
+        var metadata = 0
+        if(nbrOfChildren == 0) {
+            repeat(nbrOfMetadata) {
+                val metadataValue = scanner.nextInt()
+                metadata += metadataValue
+            }
+        } else {
+            repeat(nbrOfMetadata) {
+                val metadataRef = scanner.nextInt() - 1
+                var metadataRefValue = 0
+                if(metadataRef < children.size) {
+                    metadataRefValue = children.get(metadataRef).value
+                }
+                metadata += metadataRefValue
+            }
+        }
+        return Node(metadata, children)
+    }
+
+    private fun star2(scanner: Scanner) {
+        val rootNode = readRefValue(scanner)
+        println(rootNode.value)
     }
 }

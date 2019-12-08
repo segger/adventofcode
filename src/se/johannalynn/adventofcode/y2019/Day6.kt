4a921a53e39b656e8a44245bc495068176db340e
segger <johanna.lynn.lahti@gmail.com>
2019-12-08 01:53:25 +0100

    2019 Day6, star2

--- src/se/johannalynn/adventofcode/y2019/Day6.kt
+++ src/se/johannalynn/adventofcode/y2019/Day6.kt
@@ -6,9 +6,9 @@ object Day6 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        val scanner = Day.start(6, true)
+        val scanner = Day.start(6, false)
 
-        star1(scanner)
+        star2(scanner)
     }
 
     fun star1(scanner: Scanner) {
@@ -36,13 +36,53 @@ object Day6 {
         println(total)
     }
 
+    fun star2(scanner: Scanner) {
+        val spaceObjects = HashMap<String, SpaceObject>()
+        while(scanner.hasNextLine()) {
+            val orbit = scanner.nextLine().split(")")
+            var parent = spaceObjects.get(orbit[0])
+            if (parent == null) {
+                parent = SpaceObject(orbit[0], null)
+            }
+            var child = spaceObjects.get(orbit[1])
+            if (child == null) {
+                child = SpaceObject(orbit[1], parent)
+            } else {
+                child.parent = parent
+            }
+            spaceObjects.put(orbit[0], parent)
+            spaceObjects.put(orbit[1], child)
+        }
+
+        // my and santas inheritance
+        val me = spaceObjects.get("YOU")
+        me!!.recursiveDistanceToMe(-1)
+        val santa = spaceObjects.get("SAN")
+        santa!!.recursiveDistanceToSanta(-1)
+
+        var minDistance = Int.MAX_VALUE
+        spaceObjects.values.forEach {
+            // println("${it.name} ${it.distanceToMe} ${it.distanceToSanta}")
+            val distance = it.distanceToMe + it.distanceToSanta
+            if (distance < minDistance) {
+                minDistance = distance
+            }
+        }
+
+        println(minDistance)
+    }
+
     class SpaceObject {
         var parent: SpaceObject?
         val name: String
+        var distanceToMe: Int
+        var distanceToSanta: Int
 
         constructor(name: String, parent: SpaceObject?) {
             this.name = name
             this.parent = parent
+            this.distanceToMe = 100000
+            this.distanceToSanta = 100000
         }
 
         fun countDistanceToCOM(): Int {
@@ -50,6 +90,20 @@ object Day6 {
                 return 0
             }
             return this.parent!!.countDistanceToCOM() + 1
+        }
+
+        fun recursiveDistanceToMe(distance: Int) {
+            this.distanceToMe = distance
+            if (this.parent != null) {
+                this.parent!!.recursiveDistanceToMe(distance + 1)
+            }
+        }
+
+        fun recursiveDistanceToSanta(distance: Int) {
+            this.distanceToSanta = distance
+            if (this.parent != null) {
+                this.parent!!.recursiveDistanceToSanta(distance + 1)
+            }
         }
     }
 }

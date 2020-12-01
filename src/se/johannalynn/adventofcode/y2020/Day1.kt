befea1461157895b50a5277f68c44e65a7e67b27
segger <johanna.lynn.lahti@gmail.com>
2020-12-01 21:49:01 +0100

    2020 Day1, star2

--- src/se/johannalynn/adventofcode/y2020/Day1.kt
+++ src/se/johannalynn/adventofcode/y2020/Day1.kt
@@ -2,6 +2,7 @@ package se.johannalynn.adventofcode.y2020
 
 import se.johannalynn.adventofcode.y2020.Day.start
 import java.util.*
+import kotlin.math.exp
 
 object Day1 {
 
@@ -9,8 +10,8 @@ object Day1 {
     fun main(args: Array<String>) {
         val scanner = start(1, false)
 
-        star1(scanner)
-        //star2(scanner)
+        // star1(scanner)
+        star2(scanner)
     }
 
     private fun star1(scanner: Scanner) {
@@ -26,7 +27,39 @@ object Day1 {
     }
 
     private fun star2(scanner: Scanner) {
-
+        val input = mutableListOf<Int>()
+        while (scanner.hasNextLine()) {
+            val nbr = Integer.parseInt(scanner.nextLine())
+            input.add(nbr)
+        }
+        input.sort()
+        val expenses = input.toIntArray()
+        //expenses.forEach { println(it) }
+        var p1 = expenses.size - 1
+        var p2 = expenses.size - 2
+        var p3 = 0
+        while(p1 > 2) {
+            var sum1 = expenses[p1] + expenses[p2]
+            while (sum1 >= 2020 && p2 > p3) {
+                // println("p1 $p1, p2 $p2, p3 $p3")
+                // println("sum1 $sum1")
+                p2--
+                sum1 = expenses[p1] + expenses[p2]
+            }
+            var sum = expenses[p1] + expenses[p2] + expenses[p3]
+            while (sum <= 2020) {
+                // println("p1 $p1, p2 $p2, p3 $p3")
+                // println("SUM $sum")
+                if (sum == 2020) {
+                    println(expenses[p1] * expenses[p2] * expenses[p3])
+                }
+                p3++
+                sum = expenses[p1] + expenses[p2] + expenses[p3]
+            }
+            p1--
+            p2 = p1 - 1
+            p3 = 0
+        }
     }
 
 }

7670bf31850a405008c64cacfc715d0a6ccacd48
segger <johanna.lynn.lahti@gmail.com>
2020-12-03 07:33:47 +0100

    2018 Add day 12

---
+++ src/se/johannalynn/adventofcode/y2018/Day12.kt
@@ -0,0 +1,30 @@
+package se.johannalynn.adventofcode.y2018
+
+import java.io.File
+import java.io.FileNotFoundException
+import java.util.*
+
+object Day12 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Day12")
+
+        val inFileName = "2018/input/day12_pre.txt"
+        val scanner = Scanner(File(inFileName))
+
+        star1(scanner);
+        // star2(scanner)
+
+        scanner.close()
+    }
+
+    private fun star1(scanner: Scanner) {
+        while (scanner.hasNextLine()) {
+            scanner.nextLine()
+        }
+    }
+
+    private fun star2(scanner: Scanner) {
+    }
+}

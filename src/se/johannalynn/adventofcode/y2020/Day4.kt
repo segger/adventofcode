f5eb6be44b327309e2aa2472f5119ecbf9f81908
segger <johanna.lynn.lahti@gmail.com>
2020-12-07 20:32:54 +0100

    2020 Day4, star 2

--- src/se/johannalynn/adventofcode/y2020/Day4.kt
+++ src/se/johannalynn/adventofcode/y2020/Day4.kt
@@ -2,7 +2,6 @@ package se.johannalynn.adventofcode.y2020
 
 import se.johannalynn.adventofcode.y2020.Day.start
 import java.util.*
-import kotlin.math.exp
 
 object Day4 {
 
@@ -10,8 +9,8 @@ object Day4 {
     fun main(args: Array<String>) {
         val scanner = start(4, false)
 
-        star1(scanner)
-        // star2(scanner)
+        // star1(scanner)
+        star2(scanner)
     }
 
     private fun star1(scanner: Scanner) {
@@ -37,8 +36,51 @@ object Day4 {
         return fields.filter { it.key != "cid" }.filter { it.value }.size == 7
     }
 
-    private fun star2(scanner: Scanner) {
+    private fun checkPassport2(passport: List<String>): Boolean {
+        val fields = mutableMapOf("byr" to false, "iyr" to false, "eyr" to false, "hgt" to false, "hcl" to false, "ecl" to false, "pid" to false, "cid" to false)
+        passport.forEach {
+            val field = it.substring(0,3)
+            val value = it.substring(4).trim()
+            // println("$field => $value")
+            val valid = when(field) {
+                "cid" -> true
+                "byr" -> Integer.parseInt(value) in 1920..2002
+                "iyr" -> Integer.parseInt(value) in 2010..2020
+                "eyr" -> Integer.parseInt(value) in 2020..2030
+                "hgt" -> {
+                    val hgt = value.substring(0, value.length - 2)
+                    val unit = value.substring(value.length - 2)
+                    if (unit == "cm") {
+                        Integer.parseInt(hgt) in 150..193
+                    } else if (unit == "in") {
+                        Integer.parseInt(hgt) in 59..76
+                    } else {
+                        false
+                    }
+                }
+                "hcl" -> Regex("#[0-9a-f]{6}").containsMatchIn(value)
+                "ecl" -> value == "amb" || value == "blu" || value == "brn" || value == "gry" || value == "grn" || value == "hzl" || value == "oth"
+                "pid" -> Regex("[\\d]{9}").containsMatchIn(value) && value.length == 9
+                else -> false
+            }
+            fields[field] = valid
+        }
+        return fields.filter { it.key != "cid" }.filter { it.value }.size == 7
+    }
 
+    private fun star2(scanner: Scanner) {
+        var count = 0
+        scanner.useDelimiter("\\n\\n")
+        while (scanner.hasNext()) {
+            val passport = scanner.next()
+            //println("PASSPORT: $passport")
+            val parts = passport.split(' ','\n')
+            //println(parts.size)
+            if(checkPassport2(parts)) {
+                count++
+            }
+        }
+        println("valid: $count")
     }
 
 }

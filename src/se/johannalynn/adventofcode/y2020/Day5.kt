168f50d73c5f125033536a16800e10e047bab764
segger <johanna.lynn.lahti@gmail.com>
2020-12-07 22:28:54 +0100

    2020 Day 5, star 2

--- src/se/johannalynn/adventofcode/y2020/Day5.kt
+++ src/se/johannalynn/adventofcode/y2020/Day5.kt
@@ -16,8 +16,8 @@ BBFFBBFRLL - 820
     fun main(args: Array<String>) {
         val scanner = start(5, false)
 
-        star1(scanner)
-        // star2(scanner)
+        // star1(scanner)
+        star2(scanner)
     }
 
     private fun star1(scanner: Scanner) {
@@ -57,7 +57,26 @@ BBFFBBFRLL - 820
     }
 
     private fun star2(scanner: Scanner) {
-
+        val seatList = mutableListOf<Int>()
+        while(scanner.hasNextLine()) {
+            val input = scanner.nextLine()
+            val row = input.substring(0, input.length - 3)
+            val rowNum = calc(127, 0, 'F', 'B', row)
+            val column = input.substring(input.length - 3)
+            val colNum = calc(7, 0, 'L', 'R', column)
+            val seatID = rowNum * 8 + colNum
+            seatList.add(seatID)
+        }
+        seatList.sort()
+        var before = seatList.first()
+        var idx = 1
+        while (idx < seatList.size) {
+            if(seatList[idx] != (before + 1)) {
+                println(seatList[idx] - 1)
+            }
+            before = seatList[idx]
+            idx++
+        }
     }
 
 }

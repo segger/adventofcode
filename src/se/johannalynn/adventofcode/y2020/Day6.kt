d15183b6e6bc7b3343c550e582b1bb23fcde6189
segger <johanna.lynn.lahti@gmail.com>
2020-12-08 17:37:44 +0100

    2020 Day6, star 2

--- src/se/johannalynn/adventofcode/y2020/Day6.kt
+++ src/se/johannalynn/adventofcode/y2020/Day6.kt
@@ -9,8 +9,8 @@ object Day6 {
     fun main(args: Array<String>) {
         val scanner = start(6, false)
 
-        star1(scanner)
-        // star2(scanner)
+        // star1(scanner)
+        star2(scanner)
     }
 
     private fun star1(scanner: Scanner) {
@@ -34,8 +34,30 @@ object Day6 {
         return questions.size
     }
 
-    private fun star2(scanner: Scanner) {
+    private fun calc2(answers: List<String>): Int {
+        var questions = mutableSetOf<Char>()
+        var ch = 'a'
+        while (ch <= 'z') {
+            questions.add(ch)
+            ++ch
+        }
 
+        answers.forEach {answer ->
+            questions = questions.intersect(answer.toSet()).toMutableSet()
+        }
+        return questions.size
+    }
+
+    private fun star2(scanner: Scanner) {
+        var sum = 0
+        scanner.useDelimiter("\\n\\n")
+        while(scanner.hasNext()) {
+            val input = scanner.next()
+            val answers = input.split('\n')
+            val size = calc2(answers)
+            sum += size
+        }
+        println(sum)
     }
 
 }

173027c2bb5e0adb012c4825e77f02fcf52a9a2b
segger <johanna.lynn.lahti@gmail.com>
2023-12-10 19:03:36 +0100

    Read whole file as text

--- src/se/johannalynn/adventofcode/y2023/Day.kt
+++ src/se/johannalynn/adventofcode/y2023/Day.kt
@@ -14,4 +14,10 @@ object Day {
         val inFileName = "input/2023/day" + day + pre + ".txt"
         return Scanner(File(inFileName))
     }
+
+    fun file(day: Int, usePre: Boolean): String {
+        println("Day" + day)
+        val fileName = if (usePre) "input/2023/day${day}_pre.txt" else "input/2023/day${day}.txt"
+        return File(fileName).inputStream().bufferedReader().use { it.readText() }
+    }
 }
